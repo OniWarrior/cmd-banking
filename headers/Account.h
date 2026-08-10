3c71b5557356ab596390708945c77a61e34e11ec
OniWarrior <aranda.stephen88@gmail.com>
2026-08-09 22:08:56 -0500

    initial entry point and declaration

---
+++ headers/Account.h
@@ -0,0 +1,12 @@
+/*
+* Author : Stephen Aranda
+* File   : Account.h
+* Desc   : File for the declaration of Account class for a customer
+* Date   : 8/9/26
+*/
+
+#ifndef __Account__
+#define __Account__
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+* Author: Stephen Aranda
+* Date  : 8/9/2026
+* File  : main.cpp
+* Desc  : Entry point for cmd-banking app
+ */
+
+int main() {
+
+
+
+	return 0;
+}
