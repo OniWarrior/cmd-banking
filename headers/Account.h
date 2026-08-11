b543465d4f6959ca067a7e3ef0bc57d65bb8a14e
OniWarrior <aranda.stephen88@gmail.com>
2026-08-10 19:33:19 -0500

    entry point commit

--- headers/Account.h
+++ headers/Account.h
@@ -7,6 +7,18 @@
 
 #ifndef __Account__
 #define __Account__
+#include<string>
+
+class Account {
+private:
+	int m_cid;                      // customer id
+	std::string m_first_name;      //  customer first name
+	std::string m_last_name;      //   customer last name
+	std::string m_addr;          //    customer address
+	std::string m_phone_num;    //     customer phone number
+	std::string m_email_addr;  //      customer email address
+
+};
 
 
 #endif
