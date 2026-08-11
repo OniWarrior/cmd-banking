c13c82fa4c8c5f42e870757a4c07e8ddb5d03f2d
OniWarrior <aranda.stephen88@gmail.com>
2026-08-10 19:47:50 -0500

    added data attributes

--- headers/Customer.h
+++ headers/Customer.h
@@ -1,15 +1,15 @@
 /*
 * Author : Stephen Aranda
-* File   : Account.h
-* Desc   : File for the declaration of Account class for a customer
+* File   : Customer.h
+* Desc   : File for the declaration of Customer class for a customer
 * Date   : 8/9/26
 */
 
-#ifndef __Account__
-#define __Account__
+#ifndef __Customer__
+#define __Customer__
 #include<string>
 
-class Account {
+class Customer {
 private:
 	int m_cid;                      // customer id
 	std::string m_first_name;      //  customer first name
@@ -17,6 +17,18 @@ private:
 	std::string m_addr;          //    customer address
 	std::string m_phone_num;    //     customer phone number
 	std::string m_email_addr;  //      customer email address
+public:
+
+	Customer(Customer customer);
+
+	// accessors
+	int get_cid();
+	std::string get_first_name();
+	std::string get_last_name();
+	std::string get_addr();
+	std::string get_phone_num();
+	std::string get_email_addr();
+
 
 };
 
